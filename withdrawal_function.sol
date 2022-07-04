69141808a99872ee128cd54a32b37c287a1a97b1
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-07-04 15:05:09 +0530

    Create withdrawal_function.sol

---
+++ withdrawal_function.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.1;
+
+contract SendMoneyExample {
+
+    uint public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdrawMoney() public {
+        address payable to = payable(msg.sender);
+        to.transfer(getBalance());
+    }
+//create a function that takes on address as argument solidity 
+    function withdrawMoneyTo(address payable _to) public {
+        _to.transfer(getBalance());
+    }
+}  
+
